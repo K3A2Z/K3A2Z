c8a380f02e4e3248eb72b2d9dd57bc34ad23d059
K3A2Z <127292256+K3A2Z@users.noreply.github.com>
2023-03-08 12:59:30 +0300

    Update sedthon.py

--- sedthon.py
+++ sedthon.py
@@ -575,7 +575,7 @@ async def _(event):
 ☆ البنك : `{ms}`
 ☆ التاريخ : `{m9zpi}`
 ☆ الايدي : `{event.sender_id}`
-☆ المطور : @alhggkh
+☆ المطور : @alhgglh
 ☆ KAZ Source : @K3A2Z**
 ''')
 
@@ -688,7 +688,7 @@ async def _(event):
     photo = await sedthon.get_profile_photos(DEVS[0])
     await sedthon.send_file(event.chat_id, photo, caption=f'''
    The best !
-       -@alhggkh
+       -@alhgglh
 ''', reply_to=event)
 
 
